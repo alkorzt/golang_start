e1c913a4140310f19887fa27ded9518d7d5c8915
neo <neo@example.ru>
2019-11-17 16:34:52 +0300

    post

--- 003_http/start/main.go
+++ 003_http/start/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Person - контакт
@@ -24,14 +26,20 @@ func allContacts(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(contacts)
 }
 
+func testPostContacts(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "test...")
+}
+
 func homeIndex(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "it work...")
 }
 
 func handleRequest() {
-	http.HandleFunc("/", homeIndex)
-	http.HandleFunc("/contacts", allContacts)
-	log.Fatal(http.ListenAndServe(":5555", nil))
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homeIndex)
+	myRouter.HandleFunc("/contacts", allContacts).Methods("GET")
+	myRouter.HandleFunc("/contacts", testPostContacts).Methods("POST")
+	log.Fatal(http.ListenAndServe(":5555", myRouter))
 }
 
 func main() {
