62b7a8dece0c3810631ad71ee8962f817d342337
neo <neo@example.ru>
2019-11-17 15:13:42 +0300

    log_json

--- 003_http/start/main.go
+++ 003_http/start/main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
+
+// Person - контакт
+type Person struct {
+	Name         string `json:"Name"`
+	Surname      string `json:"Surname"`
+	Patronomical string `json:"Patronomical"`
+}
+
+// PersonBook -  контактная книга
+type PersonBook []Person
+
+func allContacts(w http.ResponseWriter, r *http.Request) {
+	contacts := PersonBook{
+		Person{Name: "Ivan", Surname: "Smirnov", Patronomical: "Petrovich"},
+	}
+	json.NewEncoder(w).Encode(contacts)
+}
 
 func homeIndex(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "it work...")
@@ -11,7 +30,8 @@ func homeIndex(w http.ResponseWriter, r *http.Request) {
 
 func handleRequest() {
 	http.HandleFunc("/", homeIndex)
-	http.ListenAndServe(":5555", nil)
+	http.HandleFunc("/contacts", allContacts)
+	log.Fatal(http.ListenAndServe(":5555", nil))
 }
 
 func main() {
